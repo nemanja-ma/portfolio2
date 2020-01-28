47904d0330a6c08cfb62d55924b40cf2986c9f21
nemanja-ma <nemanja.front@gmail.com>
2020-01-29 00:07:24 +0100

    stats

--- js/script.js
+++ js/script.js
@@ -8,25 +8,44 @@ $(document).ready(function(){
         showCursor: false
     });
 
-    jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 3000
-
-    $('.owl-carousel').owlCarousel({
-        loop:true,
-        margin:10,
-        nav:true,
-        responsive:{
-            0:{
-                items:1
-            },
-            600:{
-                items:3
-            },
-            1000:{
-                items:5
-            }
-        }
-    })
     
+    
+    var skillsTopOffset = $('.skillsSection').offset().top;
+    $(window).scroll(function(){
+        if (window.pageYOffset > skillsTopOffset - $(window).height()+400) {
+            
+            $('.owl-carousel').owlCarousel({
+                loop:true,
+                margin: 50,
+                nav:true,
+                responsive:{
+                    0:{
+                        items:1
+                    },
+                    480:{
+                        items:2
+                    },
+                    768:{
+                        items:3
+                    },
+                    938:{
+                        items:4
+                    }
+                }
+            })
+
+            jQuery.fn.carousel.Constructor.TRANSITION_DURATION = 3000
+
+            $('.chart').easyPieChart({
+            animate: ({ duration: 2000, enabled: true }),
+            onStep: function(from, to, percent) {
+                $(this.el).find('.percent').text(Math.round(percent))
+            }
+            });
+        }
+    });
+
+
 });
 
     
