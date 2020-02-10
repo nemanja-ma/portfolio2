7386ae111f575b8969a559adfa9b1d7de4828dc0
nemanja-ma <nemanja.front@gmail.com>
2020-02-10 23:46:12 +0100

    ...

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './js/script.js'
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'js/bundle.js'
-    }
-};
